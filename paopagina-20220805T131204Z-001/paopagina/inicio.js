eba0cb7a0e0cd4cde401b279ff51b0f248be65b4
Paola <pgelvezandrea@gmail.com>
2023-03-16 12:40:10 -0500

    primeros avances de pagina objetivos de desarrollo sostenible a modo de aprendizaje

---
+++ paopagina-20220805T131204Z-001/paopagina/inicio.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+ menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
